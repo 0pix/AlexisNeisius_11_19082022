23038b0d2b2d46fac30f4a1ff3900ee9c2250069
0pix <opixgamer@gmail.com>
2022-08-19 22:43:54 +0200

    Data and setup done

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+import Home from './roots/home/Home'
+import About from './roots/about/About'
+import './App.css'
+import { useState, useEffect } from 'react'
+import Header from './components/Header'
 
 function App() {
+  const [data, setData] = useState([])
+
+  useEffect(() => {
+    fetch('./data/data.json')
+      .then(function (res) {
+        return res.json()
+      })
+      .then(function (data) {
+        // store Data in State Data Variable
+        setData(data)
+      })
+      .catch(function (err) {
+        console.log(err, ' error')
+      })
+  }, [])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <Router>
+      <Header />
+      <Switch>
+        <Route exact path="/">
+          <Home data={data} />
+        </Route>
+        <Route path="/apropos">
+          <About />
+        </Route>
+      </Switch>
+    </Router>
+  )
 }
 
-export default App;
+export default App

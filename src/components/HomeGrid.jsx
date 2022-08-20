b350c0f9fc42c12d3050796f36f899d5b67d33ca
0pix <opixgamer@gmail.com>
2022-08-20 06:07:26 +0200

    #

--- src/components/HomeGrid.jsx
+++ src/components/HomeGrid.jsx
@@ -1,8 +1,6 @@
 import Thumbs from './Thumbs'
 import './HomeGrid.css'
 const HomeGrid = ({ data }) => {
-  // const tabler = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-
   return data.map((elm) => (
     <div className="Thumbs">
       <Thumbs key={elm.id} image={elm.cover} title={elm.title} />
